e6e59848938ae928d42bd609b4b4df0f383ad9d3
Fyke Lleva <110708999+f9ki3@users.noreply.github.com>
2024-05-23 09:35:42 +0800

    star-dew app bachelors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+// Define the bachelors array
+const bachelors = [
+  {
+    name: 'Alex',
+    img: require('./stardew_images/bachelors/Alex.png')
+  },
+  {
+    name: 'Elliot',
+    img: require('./stardew_images/bachelors/Elliott.png')
+  },
+  {
+    name: 'Harvey',
+    img: require('./stardew_images/bachelors/Harvey.png')
+  },
+  {
+    name: 'Sam',
+    img: require('./stardew_images/bachelors/Sam.png')
+  }
+];
+
+// Function to render a name
+function Name({ name }) {
+  return <h1>{name}</h1>;
+}
+
+// Function to render an image
+function Image({ img }) {
+  return <img src={img} alt="" />;
+}
+
+// BachelorName component to render all names and images
+export default function BachelorName() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className='bachelors'>
+      {bachelors.map((bachelor, index) => (
+        <div key={index}>
+          <Name name={bachelor.name} />
+          <Image img={bachelor.img} />
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
